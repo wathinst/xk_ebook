27ad3ba9c6bbbb003dac41848503b2f87a711d50
韦行志 <wxz@xkzhineng.com>
2018-07-19 12:10:01 +0800

    implementation curl page

---
+++ app/src/main/java/com/wxz/ebook/curlUI/CurlPage.java
@@ -0,0 +1,223 @@
+/*
+   Copyright 2012 Harri Smatt
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.wxz.ebook.curlUI;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.RectF;
+
+/**
+ * Storage class for page textures, blend colors and possibly some other values
+ * in the future.
+ * 页面纹理、混合颜色以及将来可能的某些其他值的存储类
+ * 
+ * @author harism
+ */
+public class CurlPage {
+
+	public static final int SIDE_BACK = 2;
+	public static final int SIDE_BOTH = 3;
+	public static final int SIDE_FRONT = 1;
+
+	private int mColorBack;
+	private int mColorFront;
+	private Bitmap mTextureBack;
+	private Bitmap mTextureFront;
+	private boolean mTexturesChanged;
+
+	/**
+	 * Default constructor.
+	 */
+	public CurlPage() {
+		reset();
+	}
+
+	/**
+	 * Getter for color.
+	 * 获取颜色
+	 */
+	public int getColor(int side) {
+		switch (side) {
+		case SIDE_FRONT:
+			return mColorFront;
+		default:
+			return mColorBack;
+		}
+	}
+
+	/**
+	 * Calculates the next highest power of two for a given integer.
+	 * 计算给定整数下一个最大 2 的幂值。
+	 */
+	private int getNextHighestPO2(int n) {
+		n -= 1;
+		n = n | (n >> 1);
+		n = n | (n >> 2);
+		n = n | (n >> 4);
+		n = n | (n >> 8);
+		n = n | (n >> 16);
+		n = n | (n >> 32);
+		return n + 1;
+	}
+
+	/**
+	 * Generates nearest power of two sized Bitmap for give Bitmap. Returns this
+	 * new Bitmap using default return statement + original texture coordinates
+	 * are stored into RectF.
+	 * 生成给定图像的位图大小最接近 2 的幂的值
+	 * 返回一个新的位图，使用默认返回语句 +  存储在 RectF 中的原始纹理坐标
+	 */
+	private Bitmap getTexture(Bitmap bitmap, RectF textureRect) {
+		// Bitmap original size. 位图原始大小
+		int w = bitmap.getWidth();
+		int h = bitmap.getHeight();
+		// Bitmap size expanded to next power of two. This is done due to
+		// the requirement on many devices, texture width and height should
+		// be power of two.
+		// 位图大小扩展为 2 的下一个幂，这是由于许多设备的要求，纹理宽度和高度应该是2的幂数。
+		int newW = getNextHighestPO2(w);
+		int newH = getNextHighestPO2(h);
+
+		// TODO: Is there another way to create a bigger Bitmap and copy
+		// original Bitmap to it more efficiently? Immutable bitmap anyone?
+		// TODO: 还有其他方法可以创建和复制更大的位图
+		// 原来的位图更有效?不变的位图吗?
+		Bitmap bitmapTex = Bitmap.createBitmap(newW, newH, bitmap.getConfig());
+		Canvas c = new Canvas(bitmapTex);
+		c.drawBitmap(bitmap, 0, 0, null);
+
+		// Calculate final texture coordinates.
+		// 计算最终的纹理坐标。
+		float texX = (float) w / newW;
+		float texY = (float) h / newH;
+		textureRect.set(0f, 0f, texX, texY);
+
+		return bitmapTex;
+	}
+
+	/**
+	 * Getter for textures. Creates Bitmap sized to nearest power of two, copies
+	 * original Bitmap into it and returns it. RectF given as parameter is
+	 * filled with actual texture coordinates in this new upscaled texture
+	 * Bitmap.
+	 * 获取纹理，创建位图大小为最近的 2 的幂值。复制原始位图到其中并返回，
+	 * RectF作为参数，以真实的纹理坐标填充在这个新的缩放纹理坐标中。
+	 */
+	public Bitmap getTexture(RectF textureRect, int side) {
+		switch (side) {
+		case SIDE_FRONT:
+			return getTexture(mTextureFront, textureRect);
+		default:
+			return getTexture(mTextureBack, textureRect);
+		}
+	}
+
+	/**
+	 * Returns true if textures have changed. 如果纹理改变将返回 true
+	 */
+	public boolean getTexturesChanged() {
+		return mTexturesChanged;
+	}
+
+	/**
+	 * Returns true if back siding texture exists and it differs from front
+	 * facing one.
+	 * 如果背面纹理存在，并与正面不同，返回 true
+	 */
+	public boolean hasBackTexture() {
+		return !mTextureFront.equals(mTextureBack);
+	}
+
+	/**
+	 * Recycles and frees underlying Bitmaps. 回收和释放下面的位图
+	 */
+	public void recycle() {
+		if (mTextureFront != null) {
+			mTextureFront.recycle();
+		}
+		mTextureFront = Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565);
+		mTextureFront.eraseColor(mColorFront);
+		if (mTextureBack != null) {
+			mTextureBack.recycle();
+		}
+		mTextureBack = Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565);
+		mTextureBack.eraseColor(mColorBack);
+		mTexturesChanged = false;
+	}
+
+	/**
+	 * Resets this CurlPage into its initial state. 将此曲面页重置为初始状态
+	 */
+	public void reset() {
+		mColorBack = Color.WHITE;
+		mColorFront = Color.WHITE;
+		recycle();
+	}
+
+	/**
+	 * Setter blend color. 设置混合颜色
+	 */
+	public void setColor(int color, int side) {
+		switch (side) {
+		case SIDE_FRONT:
+			mColorFront = color;
+			break;
+		case SIDE_BACK:
+			mColorBack = color;
+			break;
+		default:
+			mColorFront = mColorBack = color;
+			break;
+		}
+	}
+
+	/**
+	 * Setter for textures. 设置纹理
+	 */
+	public void setTexture(Bitmap texture, int side) {
+		if (texture == null) {
+			texture = Bitmap.createBitmap(1, 1, Bitmap.Config.RGB_565);
+			if (side == SIDE_BACK) {
+				texture.eraseColor(mColorBack);
+			} else {
+				texture.eraseColor(mColorFront);
+			}
+		}
+		switch (side) {
+		case SIDE_FRONT:
+			if (mTextureFront != null)
+				mTextureFront.recycle();
+			mTextureFront = texture;
+			break;
+		case SIDE_BACK:
+			if (mTextureBack != null)
+				mTextureBack.recycle();
+			mTextureBack = texture;
+			break;
+		case SIDE_BOTH:
+			if (mTextureFront != null)
+				mTextureFront.recycle();
+			if (mTextureBack != null)
+				mTextureBack.recycle();
+			mTextureFront = mTextureBack = texture;
+			break;
+		}
+		mTexturesChanged = true;
+	}
+
+}
---
+++ app/src/main/java/com/wxz/ebook/curlUI/CurlRenderer.java
@@ -0,0 +1,281 @@
+/*
+   Copyright 2012 Harri Smatt
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+package com.wxz.ebook.curlUI;
+
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.graphics.RectF;
+import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
+
+import java.util.Vector;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Actual renderer class.
+ * 实际渲染类
+ * @author harism
+ */
+public class CurlRenderer implements GLSurfaceView.Renderer {
+
+	// Constant for requesting left page rect. 左页矩形请求的常量
+	public static final int PAGE_LEFT = 1;
+	// Constant for requesting right page rect. 右页矩形请求的常量
+	public static final int PAGE_RIGHT = 2;
+	// Constants for changing view mode. 改变视图模式的常量
+	public static final int SHOW_ONE_PAGE = 1;
+	public static final int SHOW_TWO_PAGES = 2;
+	// Set to true for checking quickly how perspective projection looks.
+	// 设置为 true 快速检测透视投影的外观
+	private static final boolean USE_PERSPECTIVE_PROJECTION = false;
+	// Background fill color. 背景填充颜色
+	private int mBackgroundColor;
+	// Curl meshes used for static and dynamic rendering.
+	// 卷曲网格用于静态和动态的渲染
+	private Vector<CurlMesh> mCurlMeshes;
+	private RectF mMargins = new RectF();
+	private CurlRenderer.Observer mObserver;
+	// Page rectangles.  页面矩形
+	private RectF mPageRectLeft;
+	private RectF mPageRectRight;
+	// View mode. 显示模式
+	private int mViewMode = SHOW_ONE_PAGE;
+	// Screen size. 屏幕大小
+	private int mViewportWidth, mViewportHeight;
+	// Rect for render area. 渲染区域矩形
+	private RectF mViewRect = new RectF();
+
+	/**
+	 * Basic constructor.
+	 * 基础构造函数
+	 */
+	public CurlRenderer(CurlRenderer.Observer observer) {
+		mObserver = observer;
+		mCurlMeshes = new Vector<CurlMesh>();
+		mPageRectLeft = new RectF();
+		mPageRectRight = new RectF();
+	}
+
+	/**
+	 * Adds CurlMesh to this renderer.
+	 * 添加该渲染的卷曲网格
+	 */
+	public synchronized void addCurlMesh(CurlMesh mesh) {
+		removeCurlMesh(mesh);
+		mCurlMeshes.add(mesh);
+	}
+
+	/**
+	 * Returns rect reserved for left or right page. Value page should be
+	 * PAGE_LEFT or PAGE_RIGHT.
+	 * 返回左右页保留的矩形，page 的值应该为 PAGE_LEFT 或 PAGE_RIGHT.
+	 */
+	public RectF getPageRect(int page) {
+		if (page == PAGE_LEFT) {
+			return mPageRectLeft;
+		} else if (page == PAGE_RIGHT) {
+			return mPageRectRight;
+		}
+		return null;
+	}
+
+	@Override
+	public synchronized void onDrawFrame(GL10 gl) {
+
+		mObserver.onDrawFrame();
+
+		gl.glClearColor(Color.red(mBackgroundColor) / 255f,
+				Color.green(mBackgroundColor) / 255f,
+				Color.blue(mBackgroundColor) / 255f,
+				Color.alpha(mBackgroundColor) / 255f);
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		gl.glLoadIdentity();
+
+		if (USE_PERSPECTIVE_PROJECTION) {
+			gl.glTranslatef(0, 0, -6f);
+		}
+
+		for (int i = 0; i < mCurlMeshes.size(); ++i) {
+			mCurlMeshes.get(i).onDrawFrame(gl);
+		}
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		gl.glViewport(0, 0, width, height);
+		mViewportWidth = width;
+		mViewportHeight = height;
+
+		float ratio = (float) width / height;
+		mViewRect.top = 1.0f;
+		mViewRect.bottom = -1.0f;
+		mViewRect.left = -ratio;
+		mViewRect.right = ratio;
+		updatePageRects();
+
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
+		if (USE_PERSPECTIVE_PROJECTION) {
+			GLU.gluPerspective(gl, 20f, (float) width / height, .1f, 100f);
+		} else {
+			GLU.gluOrtho2D(gl, mViewRect.left, mViewRect.right,
+					mViewRect.bottom, mViewRect.top);
+		}
+
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		gl.glClearColor(0f, 0f, 0f, 1f);
+		gl.glShadeModel(GL10.GL_SMOOTH);
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+		gl.glHint(GL10.GL_LINE_SMOOTH_HINT, GL10.GL_NICEST);
+		gl.glHint(GL10.GL_POLYGON_SMOOTH_HINT, GL10.GL_NICEST);
+		gl.glEnable(GL10.GL_LINE_SMOOTH);
+		gl.glDisable(GL10.GL_DEPTH_TEST);
+		gl.glDisable(GL10.GL_CULL_FACE);
+
+		mObserver.onSurfaceCreated();
+	}
+
+	/**
+	 * Removes CurlMesh from this renderer.
+	 * 移除该渲染的卷曲网格
+	 */
+	public synchronized void removeCurlMesh(CurlMesh mesh) {
+		while (mCurlMeshes.remove(mesh))
+			;
+	}
+
+	/**
+	 * Change background/clear color.
+	 * 改变背景/清除颜色
+	 */
+	public void setBackgroundColor(int color) {
+		mBackgroundColor = color;
+	}
+
+	/**
+	 * Set margins or padding. Note: margins are proportional. Meaning a value
+	 * of .1f will produce a 10% margin.
+	 * 设置 margins 或 padding 。注意：margins 是成比例的，其值为 .1f 时意味着将生成 10% margin 。
+	 */
+	public synchronized void setMargins(float left, float top, float right,
+			float bottom) {
+		mMargins.left = left;
+		mMargins.top = top;
+		mMargins.right = right;
+		mMargins.bottom = bottom;
+		updatePageRects();
+	}
+
+	/**
+	 * Sets visible page count to one or two. Should be either SHOW_ONE_PAGE or
+	 * SHOW_TWO_PAGES.
+	 * 设置显示页面数为 1 或 2 。应该为 SHOW_ONE_PAGE 或 SHOW_TWO_PAGES 其中的一个。
+	 */
+	public synchronized void setViewMode(int viewMode) {
+		if (viewMode == SHOW_ONE_PAGE) {
+			mViewMode = viewMode;
+			updatePageRects();
+		} else if (viewMode == SHOW_TWO_PAGES) {
+			mViewMode = viewMode;
+			updatePageRects();
+		}
+	}
+
+	/**
+	 * Translates screen coordinates into view coordinates.
+	 * 将屏幕坐标转换为视图坐标。
+	 */
+	public void translate(PointF pt) {
+		pt.x = mViewRect.left + (mViewRect.width() * pt.x / mViewportWidth);
+		pt.y = mViewRect.top - (-mViewRect.height() * pt.y / mViewportHeight);
+	}
+
+	/**
+	 * Recalculates page rectangles.
+	 * 重新计算矩形页面。
+	 */
+	private void updatePageRects() {
+		if (mViewRect.width() == 0 || mViewRect.height() == 0) {
+			return;
+		} else if (mViewMode == SHOW_ONE_PAGE) {
+			mPageRectRight.set(mViewRect);
+			mPageRectRight.left += mViewRect.width() * mMargins.left;
+			mPageRectRight.right -= mViewRect.width() * mMargins.right;
+			mPageRectRight.top += mViewRect.height() * mMargins.top;
+			mPageRectRight.bottom -= mViewRect.height() * mMargins.bottom;
+
+			mPageRectLeft.set(mPageRectRight);
+			mPageRectLeft.offset(-mPageRectRight.width(), 0);
+
+			int bitmapW = (int) ((mPageRectRight.width() * mViewportWidth) / mViewRect
+					.width());
+			int bitmapH = (int) ((mPageRectRight.height() * mViewportHeight) / mViewRect
+					.height());
+			mObserver.onPageSizeChanged(bitmapW, bitmapH);
+		} else if (mViewMode == SHOW_TWO_PAGES) {
+			mPageRectRight.set(mViewRect);
+			mPageRectRight.left += mViewRect.width() * mMargins.left;
+			mPageRectRight.right -= mViewRect.width() * mMargins.right;
+			mPageRectRight.top += mViewRect.height() * mMargins.top;
+			mPageRectRight.bottom -= mViewRect.height() * mMargins.bottom;
+
+			mPageRectLeft.set(mPageRectRight);
+			mPageRectLeft.right = (mPageRectLeft.right + mPageRectLeft.left) / 2;
+			mPageRectRight.left = mPageRectLeft.right;
+
+			int bitmapW = (int) ((mPageRectRight.width() * mViewportWidth) / mViewRect
+					.width());
+			int bitmapH = (int) ((mPageRectRight.height() * mViewportHeight) / mViewRect
+					.height());
+			mObserver.onPageSizeChanged(bitmapW, bitmapH);
+		}
+	}
+
+	/**
+	 * Observer for waiting render engine/state updates.
+	 * 等待渲染引擎/状态更新的观察者。
+	 */
+	public interface Observer {
+		/**
+		 * Called from onDrawFrame called before rendering is started. This is
+		 * intended to be used for animation purposes.
+		 * onDrawFrame的调用在开始调用着色之前，这将打算用于动画的目的
+		 */
+		public void onDrawFrame();
+
+		/**
+		 * Called once page size is changed. Width and height tell the page size
+		 * in pixels making it possible to update textures accordingly.
+		 * 页面大小改变后调用，Width 和 height 表示页面的像素大小，使其可以更新相应的纹理。
+		 */
+		public void onPageSizeChanged(int width, int height);
+
+		/**
+		 * Called from onSurfaceCreated to enable texture re-initialization etc
+		 * what needs to be done when this happens.
+		 * 从 onSurfaceCreated 调用，使能纹理重新初始化等，当这种情况发生时需要做什么。
+		 */
+		public void onSurfaceCreated();
+	}
+}

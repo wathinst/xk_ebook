0ab25717c0a147aa63e4d3f4a139e7ee65b963fd
韦行志 <wxz@xkzhineng.com>
2018-12-03 12:05:13 +0800

    优化了启动瞬间的短暂空白

--- app/src/main/java/com/wxz/ebook/view/activity/SplashActivity.java
+++ app/src/main/java/com/wxz/ebook/view/activity/SplashActivity.java
@@ -3,6 +3,8 @@ package com.wxz.ebook.view.activity;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
@@ -22,16 +24,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
-
         setContentView(R.layout.activity_splash);
-
-        ImageView imageView= (ImageView)findViewById(R.id.lead_pic);
-        imageView.setImageResource(R.drawable.wlecom);
-        View view=findViewById(R.id.lead_in);
-        view.setVisibility(View.GONE);
-
 
         handler.postDelayed(new Runnable() {
             @Override
